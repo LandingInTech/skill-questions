e73681da002d6853614734445a5a2fb4e1fca7e0
FabioRosado <fabioglrosado@gmail.com>
2020-07-22 19:26:06 +0100

    Mention the command and trigger question on text question for

--- __init__.py
+++ __init__.py
@@ -3,7 +3,7 @@ import aiohttp
 import json
 
 from opsdroid.skill import Skill
-from opsdroid.matchers import match_regex
+from opsdroid.matchers import match_regex, match_crontab
 from opsdroid.events import Message
 
 _LOGGER = logging.getLogger(__name__)
@@ -16,11 +16,11 @@ class QuestionsSkill(Skill):
         super().__init__(opsdroid, config, *args, **kwargs)
         self.connector = self.opsdroid.get_connector("twitch")
 
-    @match_regex(r"^\!question (?P<question>.*)", case_sensitive=False)
+    @match_regex(r"\!question|question for (?P<question>.*)", case_sensitive=False, matching_condition="search")
     async def question_asked(self, message):
         """Question asked by viewer."""
         question = message.entities["question"]
-
+        _LOGGER.info(question)
         await self.opsdroid.memory.put(
             "questions",
             {"username": message.user, "question": question["value"], "isShown": False, "wasAsked": False},
@@ -31,3 +31,9 @@ class QuestionsSkill(Skill):
                 f"Thank you for your question {message.user}! I will ask this question as soon as possible."
             )
         )
+
+    @match_crontab('*/15 * * * *')
+    async def mention_question_command(self, message):
+        """Send message about question command ever 15 minutes."""
+        await self.connector.send(Message("Use the command !question to ask a question to the quest."))
+
